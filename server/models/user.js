8b114e2ef8c44a35aeb84bbfd70ea82964d4a987
ANIMESH PANDEY <animeshpandeyit@gmail.com>
2023-05-07 01:20:04 +0530

    ng new [...] ==>angular

--- server/models/user.js
+++ server/models/user.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: String,
-  password: String,
+    email: String,
+    password: String,
 });
 
 module.exports = mongoose.model("user", userSchema, "users");
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,17 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 const mongoose = require("mongoose");
-
-// const db =
-//   "mongodb+srv://animeshpandeyit:Animesh123@cluster0.nvlem2q.mongodb.net/?retryWrites=true&w=majority";
-
-// mongoose.connect(db, (err) => {
-//   if (err) {
-//     console.log("error", err);
-//   } else {
-//     console.log("connected to mongoDb");
-//   }
-// });
+const User = require("../models/user");
+const user = require("../models/user");
 
 mongoose
   .connect(
@@ -31,4 +22,119 @@ router.get("/", (req, res) => {
   res.send("From API Route");
 });
 
+//
+
+/* This code is defining a route for registering a new user. When a POST request is made to the
+"/register" endpoint, it creates a new instance of the User model with the data from the request
+body, saves it to the database, and sends the registered user data back in the response. If there is
+an error during the save operation, it logs the error to the console. */
+
+router.post("/register", (req, res) => {
+  let userData = req.body;
+  let user = new User(userData);
+
+  user
+    .save()
+    .then((registeredUser) => {
+      res.status(200).send(registeredUser);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.post("/login", (req, res) => {
+  /* `let userData = req.body;` is assigning the value of the request body to a variable called
+  `userData`. In this case, it is likely that the request body contains data submitted by a user
+  through a form or API call, which will be used to create a new user or authenticate an existing
+  user. */
+  let userData = req.body;
+
+  /* `User.findOne({ email: userData.email })` is a MongoDB query that searches for a user in the
+  database whose email matches the email provided in the `userData` object. It returns a promise
+  that resolves to the user object if a match is found, or null if no match is found. */
+  User.findOne({ email: userData.email })
+    .then((user) => {
+      if (!user) {
+        res.status(401).send("Invalid Email");
+      } else {
+        if (
+          user.password != userData.password ||
+          user.email != userData.email
+        ) {
+          res.status(401).send("Invalid email Password");
+        } else {
+          res.status(200).send(user);
+        }
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get("/events", (req, res) => {
+  const events = [
+    {
+      _id: 1,
+      name: "Event 1",
+      description: "Description for Event 1",
+      date: "2023-05-10",
+    },
+    {
+      _id: 2,
+      name: "Event 2",
+      description: "Description for Event 2",
+      date: "2023-05-15",
+    },
+    {
+      _id: 3,
+      name: "Event 3",
+      description: "Description for Event 3",
+      date: "2023-05-20",
+    },
+  ];
+  res.json(events);
+});
+
+router.get("/special", (req, res) => {
+  const events = [
+    {
+      _id: 1,
+      name: "Event 1",
+      description: "Description for Event 1",
+      date: "2023-05-10",
+    },
+    {
+      _id: 2,
+      name: "Event 2",
+      description: "Description for Event 2",
+      date: "2023-05-15",
+    },
+    {
+      _id: 3,
+      name: "Event 3",
+      description: "Description for Event 3",
+      date: "2023-05-20",
+    },
+  ];
+  res.json(events);
+});
+
 module.exports = router;
+
+// , (error, user) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     if (!user) {
+//       res.status(401).send("Invalid Email");
+//     } else {
+//       if (user.password != userData.password) {
+//         res.status(401).send("Invalid Password");
+//       } else {
+//         res.status(200).send(user);
+//       }
+//     }
+//   }
+// }
