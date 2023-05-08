d9a4d87bdb33b4714443dcb1a0685657defbefa7
ANIMESH PANDEY <animeshpandeyit@gmail.com>
2023-05-08 18:52:42 +0530

    RegisterComponent ==>
     to register a user.

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -9,6 +9,7 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing.module';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from './services/auth.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,8 +18,14 @@ import { FormsModule } from '@angular/forms';
     EventsComponent,
     SpecialEventsComponent,
   ],
-  imports: [BrowserModule, RouterModule, AppRoutingModule, FormsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   // registeredUserData = { email: '', password: '' };
   registeredUserData = { email: '', password: '' };
 
+  constructor(private _auth: AuthService, private router: Router) {}
+
+  ngOnInit(): void {}
+
   registerUser() {
+    this._auth
+      .registerUser(this.registeredUserData)
+      .subscribe({
+        next: (res) => console.log(res),
+        error: (err) => console.log(err),
+      });
     console.log(this.registeredUserData);
   }
 }
+// .subscribe(next?: ((value: any) => void) | null | undefined, error?: ((error: any) => void) | null | undefined, complete?: (() => void) | null | undefined): Subscription (+1 overload)
